967db7b948b70d0ba57618af154e946dd8e20b0b
elena-dragunova <elenak-8647@gmail.com>
2020-09-17 16:15:17 +0500

    Add Cart page.

--- src/containers/Cart/Cart.js
+++ src/containers/Cart/Cart.js
@@ -1,28 +1,112 @@
 import React, { Component } from 'react'
 import styles from './Cart.module.css'
 import { connect } from 'react-redux';
+import QuantityInput from '../../components/UI/QuantityInput/QuantityInput';
+import { deleteProduct } from '../../store/actions/cart';
+import Button from '../../components/UI/Button/Button';
+import {Link} from 'react-router-dom';
 
 class Cart extends Component {
   constructor(props) {
     super(props);
 
-    // this.state = {
-    //   cart: this.props.cart,
-    // }
+    this.state = {
+      cart: this.props.cart.cart,
+    }
+  }
+
+  handleQuantityChange(cartItem, quantity) {
+    this.setState(() => {
+      const cart = this.props.cart.cart;
+      const currentItemIndex = cart.findIndex(item => item.id === cartItem.id);
+      cart.splice(currentItemIndex, 1, {
+        ...cart[currentItemIndex],
+        quantity,
+      });
+      return {
+        cart,
+      }
+    })
+  }
+
+  removeItemHandler(cartItem) {
+    this.props.deleteProduct(cartItem);
+  }
+
+  renderCartItem(cartItem) {
+    const totalItemPrice = (cartItem.price * cartItem.quantity).toFixed(2);
+
+    return (
+      <div key={cartItem.id} className={styles.CartItem}>
+
+        <div className={styles.CartItemImage}>
+          <div className={styles.ImageWrapper}>
+            <img src={cartItem.image} alt=""/>
+          </div>
+        </div>
+
+        <div className={styles.CartItemInfo}>
+          <h5>{cartItem.name}</h5>
+          <p>{cartItem.vendor}</p>
+          <p className={styles.CartItemPrice}>$ {cartItem.price}</p>
+          <div className={styles.CartItemActions}>
+            <QuantityInput quantity={cartItem.quantity}
+                           minVal={1}
+                           maxVal={cartItem.maxVal}
+                           onChange={this.handleQuantityChange.bind(this, cartItem)}/>
+            <Button text="Remove"
+                    buttonStyle="MainButton"
+                    onClick={this.removeItemHandler.bind(this, cartItem)}/>
+          </div>
+        </div>
+
+        <div className={styles.CartItemTotal}>
+          <h4>$ {totalItemPrice}</h4>
+        </div>
+      </div>
+    )
   }
 
   render() {
+    let totalPrice = 0;
+    this.state.cart.forEach((cartItem) => {
+      totalPrice += cartItem.price * cartItem.quantity;
+    });
+
     return (
-      <div className={styles.Cart}>
-        {
-          this.props.cart.length > 0
-          ? <div>
-              {this.props.cart.map((cartItem) => (
-                <p>{ cartItem.name }</p>
-              ))}
-            </div>
-          : <p>Your cart is empty</p>
-        }
+      <div className="container">
+        <div className={styles.Cart}>
+          <h2>Shopping Cart</h2>
+
+          {
+            this.state.cart.length > 0
+              ? <div className={styles.CartTable}>
+                  <div className={styles.CartHeader}>
+                    <h4 className={styles.CartImageTitle}>Image</h4>
+                    <h4 className={styles.CartProductTitle}>Product</h4>
+                    <h4 className={styles.CartTotalTitle}>Total</h4>
+                  </div>
+                  {this.state.cart.map((cartItem) => {
+                    return this.renderCartItem(cartItem)
+                  })}
+                  <div className={styles.CartFooter}>
+                    <h4>Subtotal:
+                      <span className={styles.TotalPrice}>
+                        $ {totalPrice.toFixed(2)}
+                      </span>
+                    </h4>
+                    <p>Shipping, taxes and discounts will be calculated at checkout.</p>
+                    <Link to="/"
+                          className={styles.CheckoutBtn}>
+                      <Button text="Checkout"
+                              buttonStyle="AccentButton"
+                              onClick={this.props.onClose}/>
+                    </Link>
+                  </div>
+                </div>
+              : <p>Your cart is currently empty</p>
+          }
+        </div>
       </div>
     )
   }
@@ -30,8 +114,14 @@ class Cart extends Component {
 
 function mapStateToProps(state) {
   return {
-    cart: state.cart.cart,
+    cart: state.cart,
   }
 }
 
-export default connect(mapStateToProps)(Cart)
+function mapDispatchToProps(dispatch) {
+  return {
+    deleteProduct: (product) => dispatch(deleteProduct(product)),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Cart)
